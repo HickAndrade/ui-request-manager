7a46fa155e19e7e7072ec09b1f0a9e37ee5ce12f
HickAndrade <henrique_andrade2013@hotmail.com>
2022-09-23 08:36:38 -0300

    First dependencies to configure webpack, babel, loaders and react in the latest version. Inicializing on src/app.js.

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+
+const App = () =>{
+    return(
+        <div>
+            <h1>For configure Webpack, babel and all the fuck.</h1>
+        </div>
+    )
+}
+
+export default App
+
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+'use strict'
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    context: __dirname,
+    entry: [
+        path.join(__dirname,'src','index')
+    ],
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    devServer: {
+        historyApiFallback: true,
+        port:8070
+    },
+    module: {
+        rules: [
+            {
+                test:/\.js$/,
+                use:'babel-loader'
+            },
+            {
+                test: /\.(css|scss|sass)$/i,
+                use: ["style-loader", "css-loader","sass-loader"],
+            },
+            {
+                test: /\.(png|j?g|svg|gif)?$/,
+                use: 'file-loader'
+            }
+        ]},
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, 'index.html'),
+                filename: 'index.html'
+            })
+        ]
+};
